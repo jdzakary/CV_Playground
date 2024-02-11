7a6b784ec3a7edef93506141b8c7d690718ebbfd
Davis <jdzakary@neuralnova.net>
2024-02-10 19:54:26 -0600

    Adjust conf level

--- app/streaming/operations/object_detection.py
+++ app/streaming/operations/object_detection.py
@@ -2,7 +2,7 @@ import numpy as np
 from ultralytics import YOLO
 from ultralytics.engine.results import Results
 
-from app.video.processing import Operation
+from app.video.processing import Operation, Slider
 
 
 class Yolo(Operation):
@@ -13,6 +13,8 @@ class Yolo(Operation):
         super().__init__()
         self.__model = YOLO('yolov8n.pt')
         self.__model.cuda(0)
+        self.__conf = Slider(1, 100, 1)
+        self.params.append(self.__conf)
 
     def execute(self, frame: np.ndarray) -> np.ndarray:
         h, w, d = frame.shape
@@ -20,6 +22,8 @@ class Yolo(Operation):
         results: Results = self.__model(
             source=frame,
             device='0',
-            imgsz=(h, w)
+            imgsz=(h, w),
+            verbose=False,
+            conf=self.__conf.number / 100
         )
         return results[0].plot()
